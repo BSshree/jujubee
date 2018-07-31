93988bb00926eadea363dd30a138a09c6aafc490
BSshree <34095518+BSshree@users.noreply.github.com>
2018-07-31 12:49:11 +0530

    Header/footer conditions

--- frontend/modules/site/views/layouts/main.php
+++ frontend/modules/site/views/layouts/main.php
@@ -1,5 +1,4 @@
 <?php
-
 /* @var $this \yii\web\View */
 /* @var $content string */
 
@@ -14,31 +13,33 @@ use frontend\assets\AppAssetJujubee;
 AppAssetJujubee::register($this);
 $seo = \common\models\Seo::findMetaTags(Yii::$app->request->url);
 
-$this->registerMetaTag(['name' => 'description','content' => $seo->meta_desc]);
-$this->registerMetaTag(['name' => 'keywords','content' => $seo->meta_keyword]);
+$this->registerMetaTag(['name' => 'description', 'content' => $seo->meta_desc]);
+$this->registerMetaTag(['name' => 'keywords', 'content' => $seo->meta_keyword]);
 $this->title = $seo->meta_title ?: $this->title;
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
-<head>
-    <meta charset="<?= Yii::$app->charset ?>">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?= Html::csrfMetaTags() ?>
-    <title><?= Html::encode($this->title) ?></title>
-    <?php $this->head() ?>
-</head>
-<body>
-<?php $this->beginBody() ?>
-    <?php //echo($this->keywords); exit; ?>
-     <?php  echo $this->render('@frontend/modules/site/views/layouts/header'); ?> 
+    <head>
+        <meta charset="<?= Yii::$app->charset ?>">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <?= Html::csrfMetaTags() ?>
+        <title><?= Html::encode($this->title) ?></title>
+        <?php $this->head() ?>
+    </head>
+    <body>
+        <?php $this->beginBody() ?>
+        <?php if (Yii::$app->request->url != '/terms') { ?>
+            <?php echo $this->render('@frontend/modules/site/views/layouts/header'); ?> 
+        <?php } ?>
 
         <?= $content ?>
-   
-     <?php echo $this->render('@frontend/modules/site/views/layouts/footer'); ?> 
 
-<?php $this->endBody() ?>
-</body>
+        <?php if (Yii::$app->request->url != '/terms') { ?>
+            <?php echo $this->render('@frontend/modules/site/views/layouts/footer'); ?> 
+        <?php } ?>
+        <?php $this->endBody() ?>
+    </body>
 </html>
 <?php $this->endPage() ?>
